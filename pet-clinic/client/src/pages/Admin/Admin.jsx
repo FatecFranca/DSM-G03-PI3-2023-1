9910cc13e77b7ced3336e950044eaa171e2463c8
PedroNevesHespanhol <88465619+PedroNevesHespanhol@users.noreply.github.com>
2023-05-15 19:27:18 -0300

    ajuste admin

--- pet-clinic/client/src/pages/Admin/Admin.jsx
+++ pet-clinic/client/src/pages/Admin/Admin.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NovoVet from '../../components/Buttons/NovoVet';
 import NovoServico from '../../components/Buttons/NovoServico';
-import ClienteList from '../../components/CardClientes/ClienteList';
 import { Container, Title, AdminButtons} from './admin.styled';
 import  Cabecalho from '../../components/Header/Header'
 
