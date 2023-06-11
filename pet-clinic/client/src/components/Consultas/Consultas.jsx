375ed85a28a74664bf6b9edea3637d4d12935400
PedroNevesHespanhol <88465619+PedroNevesHespanhol@users.noreply.github.com>
2023-06-11 18:06:10 -0300

    chore: nome pet consulta

--- pet-clinic/client/src/components/Consultas/Consultas.jsx
+++ pet-clinic/client/src/components/Consultas/Consultas.jsx
@@ -5,6 +5,7 @@ import http from "../../db/http";
 
 function ConsultaList(props) {
   const [consultasData, setConsultasData] = useState([]);
+  const [petsData, setPetsData] = useState([]);
 
   const pet_id = props.petId;
 
@@ -21,8 +22,7 @@ function ConsultaList(props) {
             },
           });
 
-          setConsultasData(response.data);
-          console.log(response);
+          setConsultasData(response.data.response);
         } else {
           // Lógica para lidar com a ausência do token
         }
@@ -35,24 +35,73 @@ function ConsultaList(props) {
     fetchConsultaData();
   }, [props.setPetId, props.petId]);
 
+  // // Função para obter o nome do pet com base no ID
+  // const getPetName = (petId) => {
+  //   const pet = petsData.find((pet) => pet._id === petId);
+  //   return pet ? pet.nome : "";
+  // };
+  //GET pets para mostrar nas options
+  useEffect(() => {
+    const fetchPetsData = async () => {
+      try {
+        const token = localStorage.getItem('token_API');
+
+        if (token) {
+          const response = await http.get(`/pet/${pet_id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            }
+          });
+
+          setPetsData(response.data);
+        } else {
+          // Lógica para lidar com a ausência do token
+        }
+      } catch (error) {
+        console.error(error);
+        // Lógica para lidar com o erro da requisição
+      }
+    };
+
+    fetchPetsData();
+  }, [props.setPetId, props.petId]);
+
+  // // Função para obter o nome do veterinário com base no ID
+  // const getVetName = (vetId) => {
+  //   const vet = vetsData.find((vet) => vet._id === vetId);
+  //   return vet ? vet.nome : "";
+  // };
+
+  // Função para formatar a data e hora no formato desejado
+  const formatDateTime = (dateTime) => {
+    const options = {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return new Date(dateTime).toLocaleString("pt-BR", options);
+  };
+
   return (
     <style.TableContainer>
       <style.Table>
         <style.HeaderRow>
           <style.Cell>Data</style.Cell>
-          <style.Cell>Servico</style.Cell>
+          <style.Cell>Serviço</style.Cell>
           <style.Cell>Veterinário</style.Cell>
-          <style.Cell>Hora</style.Cell>
+          <style.Cell>Pet</style.Cell>
         </style.HeaderRow>
 
-        {/* {consultasData.map(consulta => (
-          <style.Row key={consulta._id}>
-            <style.Cell>{new Date(consulta.date_time).toLocaleDateString('pt-BR')}</style.Cell>
+        {consultasData.map(consulta => (
+          <style.Row key={consulta.pet_id}>
+            <style.Cell>{formatDateTime(consulta.date_time)}</style.Cell>
             <style.Cell>{consulta.motivo}</style.Cell>
             <style.Cell>{consulta.vet_id}</style.Cell>
             <style.Cell>{consulta.pet_id}</style.Cell>
           </style.Row>
-        ))} */}
+        ))}
       </style.Table>
     </style.TableContainer>
   );
